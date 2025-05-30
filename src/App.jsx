5254068ebbc8b82f76f6675a757b3c0a6a158311
lisa <njokuleesah@gmail.com>
2025-05-30 05:38:19 +0000

    add styled search bar with search button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-
+import SearchBar from "./components/Searchbar"
 
 function App() {
   
 
   return (
     <>
-     <h1 class="text-5xl font-bold underline">
-    Hello world!
-     </h1>
+   <SearchBar/>
+    
     </>
   )
 }
